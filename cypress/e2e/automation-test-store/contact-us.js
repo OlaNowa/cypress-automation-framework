529bc6000be69fc65ff318e626c13bed8b463b36
OlaNowa <ola.kr.nowak@gmail.com>
2023-10-13 14:07:41 +0200

    Variables and cy commands

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -5,13 +5,15 @@ describe("Test Contact Us form via Automation Test Store", () => {
     //positive path
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://automationteststore.com/")
-        cy.get("a[href$='contact']").click()
-        //cy.xpath("//a[contains(@href, 'contact')]").click();
+        cy.get("a[href$='contact']").click().then(function(linkText){
+            cy.log("clicked on link using text: " + linkText.text())
+        })
         cy.get('#ContactUsFrm_first_name').type("Alex")
         cy.get('#ContactUsFrm_email').type("alex_blogs123@test.com")
         cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email')
         cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")
         cy.get("button[title='Submit']").click()
         cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
+        cy.log("Test has completed!")
     });
 })
