c137843cfc083658f50237ff136d0a58c3311787
OlaNowa <ola.kr.nowak@gmail.com>
2023-11-02 13:39:39 +0100

    Hooks and fixtures

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,6 +1,12 @@
 /// <reference types="Cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
+    before(function() {
+        cy.fixture('example').then(function(data) {
+            //this.data = data; 
+            globalThis.data = data;  //this logic if above doesn't work
+        })
+    })
     //positive path
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
@@ -8,9 +14,9 @@ describe("Test Contact Us form via WebdriverUni", () => {
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
         // cy.get('#contact-us').click()
-        cy.get('[name="first_name"]').type("Aleksandra")
-        cy.get('[name="last_name"]').type("Smith")
-        cy.get('[name="email"]').type("aleks.smith@test.com")
+        cy.get('[name="first_name"]').type(data.first_name)
+        cy.get('[name="last_name"]').type(data.last_name)
+        cy.get('[name="email"]').type(data.email)
         cy.get('textarea.feedback-input').type("test")
         cy.get('[type="submit"]').click()
         cy.get('h1').should('have.text', 'Thank You for your Message!')
@@ -20,8 +26,8 @@ describe("Test Contact Us form via WebdriverUni", () => {
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
         //cy.get('#contact-us').click()
-        cy.get('[name="first_name"]').type("Alex")
-        cy.get('[name="last_name"]').type("Smith")
+        cy.get('[name="first_name"]').type(data.first_name)
+        cy.get('[name="last_name"]').type(data.last_name)
         cy.get('textarea.feedback-input').type("test")
         cy.get('[type="submit"]').click()
         cy.get('body').contains('Error: all fields are required')
